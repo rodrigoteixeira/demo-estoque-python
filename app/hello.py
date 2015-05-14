a00ce2a5ee8616976fe5253b100087b3932dbb36
tiagojesus <tiagojesus@gmail.com>
2015-05-14 18:21:44 +0000

    configurando o hello.py para prover arquivos estaticos

--- app/hello.py
+++ app/hello.py
@@ -1,9 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, url_for, send_from_directory
+
+app = Flask(__name__, static_folder='static', static_url_path='/ui')
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))

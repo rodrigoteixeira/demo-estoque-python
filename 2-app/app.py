1dc2022aa3872a58ff5693e7ee272c2607c8280a
Tiago Oliveira de Jesus <tiagojesus@gmail.com>
2015-05-20 12:05:47 +0000

    Listar, incluir e atualizar produtos

--- 2-app/app.py
+++ 2-app/app.py
@@ -1,12 +1,43 @@
+# coding=UTF-8
+#!flask/bin/python
+from flask import Flask, jsonify, request, abort, url_for, send_from_directory
+from produtos import RepositorioProdutos, ElementoInexistente, ElementoInvalido
+from operacoes import RepositorioOperacoes
 import os
-from flask import Flask, url_for, send_from_directory
+
+
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_400_BAD_REQUEST = 400
+HTTP_404_NOT_FOUND  = 404
 
 app = Flask(__name__, static_folder='static', static_url_path='/ui')
-
+repProdutos = RepositorioProdutos()
 
 @app.route("/")
 def hello():
     return send_from_directory(app.static_folder, 'index.html')
+    
+    
+@app.route('/app/produtos', methods=['GET'])
+def list_Produtos():
+    produtos = repProdutos.listar()
+    return jsonify({'produtos': produtos})    
+    
+@app.route('/app/produtos', methods=['POST'])
+def create_prod():
+    try:
+        if not request.json['id']:
+            print( 'adicionar' )
+            produto = repProdutos.adicionar(request.json)
+        else:
+            print( 'atualizar' )
+            produto = repProdutos.atualizar(request.json)
+            
+        return jsonify({'resultado': produto}), HTTP_201_CREATED
+    except ElementoInvalido:
+       abort(HTTP_400_BAD_REQUEST)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
--- 2-app/static/src/app.js
+++ 2-app/static/src/app.js
@@ -1,76 +1,31 @@
 (function(){
-    var PRODUTOS = [
-    {
-        "id": 1,
-        "nome":  "Blu-ray - O exterminador do Futuro",
-        "descricao": "Andróide (Arnold Schwarzenegger) vem do futuro com o objetivo de matar a mãe (Linda Hamilton) de um futuro líder guerrilherio humano.",
-        "preco": 22.41,
-		"quantidade": 10,
-		"categoria" : 'DVD'
-    },
-    {
-        "id": 2,
-        "nome": "Arrow - A Segunda Temporada Completa (5 Discos)",
-        "descricao": "Depois de se retirar para a ilha onde ficou preso uma vez, Oliver Queen retorna para a proteção de sua mãe, irmã e de sua corporação sitiada", 
-        "preco": 84.92,
-		"quantidade": 8,
-		"categoria" : 'DVD'
-    },
-    {
-        "id": 3,
-        "nome": "Exterminador de cupins 400ml",
-        "descricao": "O exterminador de cupins", 
-        "preco": 19.33,
-		"quantidade": 30,
-		"categoria" :  'limpeza'
-    },	
-	{
-        "id": 4,
-        "nome": "Fantasia Adulto Exterminador John Connor",
-        "descricao": "Transforme-se em seu heroi", 
-        "preco": 300.85,
-		"quantidade": 3,
-		"categoria" :  'fantasia'
-    }	
-];
-
-
-    angular.module('meuEstoque', [])
-        .controller('MainController', MainController)
+     angular.module('meuEstoque', [])
         .controller('ProdutoController', ProdutoController);
     
-    
-    
-    
-    function MainController(){
-        
-    }
-    
-    function ProdutoController(){
+    function ProdutoController($http, $log){
         var self = this;
-        self.produtos = PRODUTOS;
+        self.produtos = [];
         self.produtoCorrente = null;
+        
+        
+        function buscarProdutos(){
+            $http.get('/app/produtos').then(function(request){
+                $log.debug('$http.get("/app/produtos")', request);
+                self.produtos = request.data.produtos;
+            });
+        }
         
         self.novo = function(){
             self.produtoCorrente = { id:'' };
         }
         
         self.salvar = function(){
-            if(self.produtoCorrente.id){
-                var index = -1;
-                self.produtos.forEach(function(item, i){
-                    if(item.id == self.produtoCorrente.id){
-                        index = i;      
-                    }
-                });
-                if(index >= 0 ){
-                    self.produtos[index] = self.produtoCorrente;
-                }
-            }else{
-                self.produtoCorrente.id = self.produtos.length;
-                self.produtos.push(self.produtoCorrente);
-            }
-            self.produtoCorrente = null;
+            
+            $http.post('/app/produtos', self.produtoCorrente).then(function(resquest){
+                self.produtoCorrente = null;
+                buscarProdutos();
+            });
+            
         }
         
         self.editar = function(item){
@@ -80,5 +35,9 @@
         self.cancelar = function(){
             self.produtoCorrente = null;
         }
+        
+        
+        
+        buscarProdutos();
     }
 }());
